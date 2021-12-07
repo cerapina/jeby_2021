a4c82abb0fd3cfa6203ddbf4b3cc10f6d15e7c23
cerapina <cerapina11@gmail.com>
2021-12-07 14:16:56 +0900

    로그아웃 기능 개발 완료

--- mysite/scripts/runserver.bat
+++ mysite/scripts/runserver.bat
@@ -1,3 +1,3 @@
 @echo off
 cd d:/workspace/jeby_2021/mysite
-python manage.py runserver 0.0.0.0:8080 --settings=config.settings.local
+python manage.py runserver 0.0.0.0:8000 --settings=config.settings.local
